919d8099c438d45e4177cdddb6626fed6fa18dcd
VuJson <3059680677@qq.com>
2022-01-02 00:09:33 +0800

    feat: complete min heap

--- 07-graph图/02-bfs.js
+++ 07-graph图/02-bfs.js
@@ -4,12 +4,10 @@ const bfs = (n) => {
 	const queue = [n]
 	const set = new Set()
 	set.add(n)
-	console.log(graph[0], 'gra')
 	while (queue.length) {
-		console.log(queue)
 		const temp = queue.shift()
 		console.log(temp)
-		graph[n].forEach((c) => {
+		graph[temp].forEach((c) => {
 			if (!set.has(c)) {
 				set.add(c)
 				queue.push(c)
--- 08-heap堆/01-MinHeap.js
+++ 08-heap堆/01-MinHeap.js
@@ -13,14 +13,49 @@ class MinHeap {
 	shiftUp(index) {
 		if (index == 0) return
 		const parentIndex = this.getParentIndex(index)
-		if (this.heap[parentIndex] > this.heap(index)) {
+		if (this.heap[parentIndex] > this.heap[index]) {
 			this.swap(parentIndex, index)
 			this.shiftUp(parentIndex)
 		}
 	}
+	getLeftIndex(i) {
+		return i * 2 + 1
+	}
+	getRightIndex(i) {
+		return i * 2 + 2
+	}
+	shiftDown(index) {
+		const leftIndex = this.getLeftIndex(index)
+		const rightIndex = this.getRightIndex(index)
+		if (this.heap[leftIndex] < this.heap[index]) {
+			this.swap(leftIndex, index)
+			this.shiftDown(leftIndex)
+		}
+		if (this.heap[rightIndex] < this.heap[index]) {
+			this.swap(rightIndex, index)
+			this.shiftDown(rightIndex)
+		}
+	}
 	insert(value) {
 		this.heap.push(value)
-
 		this.shiftUp(this.heap.length - 1)
 	}
+	pop() {
+		this.heap[0] = this.heap.pop()
+		this.shiftDown(0)
+	}
+	// 获取堆顶
+	peek() {
+		return this.heap[0]
+	}
+	size() {
+		return this.heap.length
+	}
 }
+
+const h = new MinHeap()
+
+h.insert(3)
+h.insert(2)
+h.insert(1)
+h.pop()
